df8a66807a011b6f762440d2590299b7adb9de1d
Zouhair Ghazi <zouhair.ghazi1999@gmail.com>
2023-12-18 12:02:29 +0100

    Update problem

---
+++ MaximumSubarrayLeetcode/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example input
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        // Expected answer : 6, head to src/main/test/SolutionTest.java for more UTs
+        System.out.println(Solution.maxSubArray(nums));
+    }
+}
--- MaximumSubarrayLeetcode/src/main/java/org/example/Solution.java
+++ MaximumSubarrayLeetcode/src/main/java/org/example/Solution.java
@@ -1,2 +1,16 @@
-package org.example;public class Solution {
+package org.example;
+
+public class Solution {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
 }
