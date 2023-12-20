c12e30f6e5b5e8d4d80052eca971d5e0c49544c2
Zouhair Ghazi <zouhair.ghazi1999@gmail.com>
2023-12-20 11:35:05 +0100

    Added new problem : Array division

---
+++ ArrayDivision/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example input
+        List<Integer> nums = List.of(7, 4, 6, 1, 8, 9, 9);
+
+        // Expected answer : 5, head to src/main/test/SolutionTest.java for more UTs
+        System.out.println("Division can be done at index: " + Solution.divideArray(nums));
+    }
+}
---
+++ ArrayDivision/src/main/java/org/example/Solution.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.List;
+
+public class Solution {
+
+    public static int divideArray(List<Integer> nums) {
+
+        int n = nums.size();
+        if (n % 2 == 0)
+            throw new IllegalArgumentException("The array's length can't be pair");
+
+        int sumL = 0, sumR = nums.stream().mapToInt(Integer::intValue).sum() - nums.get(0);
+
+        for (int i = 1; i < n - 1; i++) {
+            sumL += nums.get(i - 1);
+            sumR -= nums.get(i);
+
+            if (sumL == sumR)
+                return i;
+        }
+
+        return -1; // There was no possible partition of the array respecting the given condition
+    }
+}
